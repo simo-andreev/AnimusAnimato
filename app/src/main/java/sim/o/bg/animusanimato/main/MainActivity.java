76524c1cf52e5798ab8c20d91646f4c66fdde94d
simeon.andreev <simeon.andreev@softwaregroup.com>
2018-02-07 14:14:40 +0200

    init: Initial commit, including a single simple Activity with a recycler and an exaple row item for said recycler. Oh, also set some deps, libs and vers. Cheers m9 =]

---
+++ app/src/main/java/sim/o/bg/animusanimato/main/MainActivity.java
@@ -0,0 +1,16 @@
+package sim.o.bg.animusanimato.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import sim.o.bg.animusanimato.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
